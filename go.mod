f6d344ddc06c1f1c290f5d818c3c1f7b36e26436
SolyaraDaDaYa <enderderder@gmail.com>
2025-06-21 13:35:21 +0400

    feat: добавил настройки Kafka для потребителя уведомлений

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module pinstack-notification-service
 go 1.24.2
 
 require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.10.1
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
@@ -11,6 +12,7 @@ require (
 	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.72.0
+	google.golang.org/protobuf v1.36.6
 )
 
 require (
@@ -26,6 +28,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
@@ -42,6 +45,5 @@ require (
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/delivery/kafka/consumer/notification_consumer.go
+++ internal/delivery/kafka/consumer/notification_consumer.go
@@ -2,20 +2,40 @@ package consumer
 
 import (
 	"context"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"log/slog"
 	"pinstack-notification-service/config"
 	"pinstack-notification-service/internal/logger"
 )
 
 type NotificationConsumer struct {
-	config config.KafkaConfig
-	log    *logger.Logger
+	config   config.KafkaConfig
+	log      *logger.Logger
+	consumer *kafka.Consumer
 }
 
-func NewNotificationConsumer(cfg config.KafkaConfig, log *logger.Logger) *NotificationConsumer {
-	return &NotificationConsumer{
-		config: cfg,
-		log:    log,
+func NewNotificationConsumer(cfg config.KafkaConfig, log *logger.Logger) (*NotificationConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":       cfg.Brokers,
+		"group.id":                cfg.ConsumerGroupID,
+		"auto.offset.reset":       cfg.AutoOffsetReset,
+		"enable.auto.commit":      cfg.EnableAutoCommit,
+		"auto.commit.interval.ms": cfg.AutoCommitIntervalMs,
+		"session.timeout.ms":      cfg.SessionTimeoutMs,
+		"max.poll.records":        cfg.MaxPollRecords,
+		"max.poll.interval.ms":    cfg.MaxPollIntervalMs,
+	})
+
+	if err != nil {
+		log.Error("Failed to create Kafka consumer", slog.String("error", err.Error()))
+		return nil, err
 	}
+
+	return &NotificationConsumer{
+		config:   cfg,
+		log:      log,
+		consumer: c,
+	}, nil
 }
 
 func (c *NotificationConsumer) Start(ctx context.Context) {
